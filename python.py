f664b6cbe8e18d702659cba4a53040f2cbe7849c
Smolett1974 <107723474+Smolett1974@users.noreply.github.com>
2022-06-30 15:03:55 +0300

    Revert "Sec branch"

--- python.py
+++ python.py
@@ -2,9 +2,4 @@
 # OMG Python here
 
 print('Let\'s make something cool!')
-x =int(input("input int number"))
-y = ["*"for i in range(x)]
-print("".join(y))
-    
-print('next step for you')
-print()
+
