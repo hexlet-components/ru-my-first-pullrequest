e401aa1211e430628a6317f59ee742315bc43a23
stasyurin <72629239+stasyurin@users.noreply.github.com>
2022-07-29 08:57:04 +0300

    Update bomba.py

--- bomba.py
+++ bomba.py
@@ -3,7 +3,7 @@
 i=1
 txt = 'Кирилл у Мы обречены https://youtu.be/UKjmafONRsg'
 while True:
-	name = 'Кирилл у Мы обречены' + str(i)
+	name = 'i need more ' + str(i)
 	with open(name, 'w') as f:
 		f.write(txt)
 	i+=1
