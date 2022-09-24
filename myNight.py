aeca3a542cd649d8ac5a3e37fd2a676fe0010540
getloveresponse <annna.sokk@gmail.com>
2022-09-25 03:14:42 +0600

    [#1111] add 2 files

---
+++ myNight.py
@@ -0,0 +1,8 @@
+
+
+print ("O, yes. Hexlet, you make my night ! Thanks! it' was perfect");
+# Питончик! 
+# Hexlet как тирамису. 
+# "Этот файл создан через vscode благодаря быстрой команде из терминала code . "
+
+#сейчас попробую сделать pull request 
